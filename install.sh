4f55f13001768011b2f581c8426ff467026da71e
Mihai Blebea <mihaiserban.blebea@gmail.com>
2021-10-28 23:36:38 +0100

    adds conditional db install

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 echo "installing the database"
-sqlite3 store.db < ./init.sql
+DB_FILE=./store.db
+if test -f "$DB_FILE"; then
+    echo "$DB_FILE exists. do not install db"
+else
+	echo "$DB_FILE does not exist. install db"
+	sqlite3 store.db < ./init.sql
+fi
